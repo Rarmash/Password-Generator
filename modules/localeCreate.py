dc61ca377f58017440cba2d9a8e50c188c44642e
Andrew <62994051+rarmash@users.noreply.github.com>
2021-06-03 00:36:31 +0300

    German update
    Added **German**.

--- modules/localeCreate.py
+++ modules/localeCreate.py
@@ -7,12 +7,14 @@ path = "settings.ini"
 
 def localeCreate(path):
     config.add_section("LOCALE")
-    print("Enter your language: English or Русский",sep='\n')
+    print("Enter your language: English or Русский or Deutsch",sep='\n')
     qq = input()
     if qq == "Русский" or qq == 'русский' or qq == 'russian' or qq == 'Russian':
         config.set("LOCALE", "language", "ru")
     if qq == "English" or qq == 'english' or qq == 'Английский' or qq == 'английский':
         config.set("LOCALE", "language", "en")
+    if qq == "Deutsch" or qq == 'deutsch' or qq == 'German' or qq == 'german':
+        config.set("LOCALE", "language", "de")
     
     with open(path, "w+") as config_file:
         path = "settings.ini"
